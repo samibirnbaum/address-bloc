94d3c51096b81b47e14b4167663ca938e3d99b5e
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2017-12-01 12:56:42 +0000

    C6 Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
-require_relative "entry" #pulls in the entry.rb file
+require_relative "entry" #pulls in the entry.rb file relative to my project
+require "csv" #pulls in csv class which is installed in the system
 
 class AddressBook
     attr_reader :entries
@@ -19,5 +20,15 @@ class AddressBook
         end
 
         entries.insert(index, Entry.new(name, phone_number, email)) #inserts entry object in entries array at index
+    end
+
+    def import_from_csv(file_name)
+        csv_text = File.read(file_name) #returns file as a "string" 
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true) #parses csv out of string, returns array of arrays
+
+        csv.each do |row| #iterates over every row (array)
+            row_hash = row.to_hash #uses headers as key for each column 
+            add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"]) #access that rows hash through key which is our headers in our csv file       
+        end
     end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,8 +1,15 @@
 require_relative "../models/address_book.rb"
 
 RSpec.describe "AddressBook" do
-    describe "attributes" do
-        let(:book) {AddressBook.new}
+    let(:book) {AddressBook.new}
+
+    def check_entry(entry, expected_name, expected_number, expected_email)
+        expect(entry.name).to eq(expected_name) #remember each array cell is holding an entry object with the following attributes
+        expect(entry.phone_number).to eq(expected_number)
+        expect(entry.email).to eq(expected_email)
+    end
+
+    describe "attributes" do 
         it "responds to entries" do
             expect(book).to respond_to(:entries)        
         end
@@ -15,16 +22,54 @@ RSpec.describe "AddressBook" do
     end
 
     describe "#add_entry" do
-        let(:book) {AddressBook.new}
         it "adds only one item to the address book" do
             book.add_entry("Sami", "0204648518", "sami@email.com")
             expect(book.entries.size).to eq(1)
         end
         it "adds the correct information to entries" do
             book.add_entry("Sami", "0204648518", "sami@email.com")
-            expect(book.entries[0].name).to eq("Sami")
-            expect(book.entries[0].phone_number).to eq("0204648518")
-            expect(book.entries[0].email).to eq("sami@email.com")
+            check_entry(book.entries[0], "Sami", "0204648518", "sami@email.com" )
         end
+    end
+
+    describe "#import_from_csv" do
+        it "imports the correct number of entries" do
+            book.import_from_csv("entries.csv") #call the actual method
+            book_size = book.entries.size #following import method check size of entries array
+            expect(book_size).to eq(5) #the assumption is we imported 5 entries through our csv file
+        end
+        it "imports the first entry" do
+            book.import_from_csv("entries.csv")
+            entry_one = book.entries[0]
+
+            check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+        end
+        it "imports the 2nd entry" do
+            book.import_from_csv("entries.csv")
+            entry_two = book.entries[1]
+            
+            check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+          end
+      
+          it "imports the 3rd entry" do
+            book.import_from_csv("entries.csv")
+            entry_three = book.entries[2]
+            
+            check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+          end
+      
+          it "imports the 4th entry" do
+            book.import_from_csv("entries.csv")
+            entry_four = book.entries[3]
+            
+            check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+          end
+      
+          it "imports the 5th entry" do
+            book.import_from_csv("entries.csv")
+            entry_five = book.entries[4]
+            
+            check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+          end
     end
 end
